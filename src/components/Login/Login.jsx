ceee40660729c14728f70d25f4f9de3fc3d695b7
Arif Hossan <arif.hossan.eu@gmail.com>
2023-11-17 23:31:25 +0600

    sign in and log out and toggle sign in and logout

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,17 +1,48 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
-import {GoogleAuthProvider, getAuth} from 'firebase/auth'
+import React, { useState } from 'react';
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth'
 import app from '../../firebase/firebase.init';
 
 const Login = () => {
-    const auth =getAuth(app);
+    const auth = getAuth(app);
+    // console.log(app);
+    const [user, setUser] = useState(null);
     const provider = new GoogleAuthProvider();
-    const handleGoogleSignIn = () =>{
-        console.log('Google Sign In');
+    const handleGoogleSignIn = () => {
+        signInWithPopup(auth, provider)
+            .then(result => {
+                const loggedInUser = result.user;
+                console.log(loggedInUser);
+                setUser(loggedInUser);
+            })
+            .catch(error => {
+                console.log('error', error.message);
+            })
+    }
+    const handleSignOut = () => {
+        signOut(auth)
+            .then(() => {
+                setUser(null)
+            })
+            .catch(error => {
+                console.log("error", error.message);
+            })
     }
     return (
         <div>
-            <button onClick={handleGoogleSignIn}>GoogleLogin</button>
+            {/*  user ? logOut : signIn  */}
+            {/* toggle logOut and SignIn option */}
+            {user ?
+                <button onClick={handleSignOut}>Sing Out</button> :
+                <button onClick={handleGoogleSignIn}>GoogleLogin</button>}
+            {
+                user && <div>
+                    <img src={user?.photoURL} alt="" />
+                    <h3>User : {user.displayName}</h3>
+                    <p>Email : {user.email}</p>
+                    <p>Phone : {user.phoneNumber ? user.phoneNumber : "Not Available"}</p>
+                </div>
+            }
         </div>
     );
 };
